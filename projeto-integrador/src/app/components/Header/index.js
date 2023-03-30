60d6e261d537e417d198e7e9a6d9fb253c95d1b1
Guto Silva <carlosac_silva@yahoo.com.br>
2023-03-30 18:18:39 -0300

    Criado o componente para o cabeçalho da página

--- projeto-integrador/src/app/components/Header/index.js
+++ projeto-integrador/src/app/components/Header/index.js
@@ -1,9 +1,25 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import styles from './Header.module.css';
 
 function Header() {
     return (
-        <>
-        
-        </>
+        <header>
+            <Navbar bg="dark" variant="dark" fixed="top">
+                <Container fluid className="justify-content-around">
+                    <Navbar.Brand className={styles.brand}>
+                        <span>Sistema de Controle de Entrada</span>
+                        <span>e Saída de Processos</span>
+                    </Navbar.Brand>
+                    <Nav>
+                        <Nav.Link href="#">Processos</Nav.Link>
+                        <Nav.Link href="#">Funcionários</Nav.Link>
+                        <Nav.Link href="#">Departamentos</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </header>
     );
 }
 
